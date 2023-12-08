aad61d936d185f98ec612772185f1638910d0a88
Alix Branchut <alix.branchut@outlook.fr>
2023-12-08 15:15:06 +0100

    method show()

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,11 +1,28 @@
-class Tic_tac_toe():
-    pass
+import numpy as np
 
+class Tic_tac_toe():
+    
+    def __init__(self):
+        self.grille = np.zeros((3,3))
+        self.player = -1 #1 or -1
+    
+    def show(self):
+
+        symb = lambda i, j: ' ' if self.grille[i,j] == 0 else ('X' if self.grille[i,j] == 1 else 'O')
+
+        print(f"{symb(0,0)} | {symb(0,1)} | {symb(0,2)}")
+        print("---------")
+        print(f"{symb(1,0)} | {symb(1,1)} | {symb(1,2)}")
+        print("---------")
+        print(f"{symb(2,0)} | {symb(2,1)} | {symb(2,2)}")
 
 
 if __name__ == "__main__":
+
     game = Tic_tac_toe()
+    game.show()
+    """
     while not game.is_finished():
         game.next_move(input())
         game.show()
-    game.winner()
+    game.winner()"""
