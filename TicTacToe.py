ab8277247473866cdf6c79349166eaf0f4623ef1
Alix Branchut <alix.branchut@outlook.fr>
2023-12-08 15:21:45 +0100

    methods winner and is_finished

--- TicTacToe.py
+++ TicTacToe.py
@@ -15,14 +15,33 @@ class Tic_tac_toe():
         print(f"{symb(1,0)} | {symb(1,1)} | {symb(1,2)}")
         print("---------")
         print(f"{symb(2,0)} | {symb(2,1)} | {symb(2,2)}")
+    
+    def winner(self):
+        """Returns -1, 1 for the winner or 0 if there is no winenr yet.
+        """
+        for k in range(3):
+            if np.sum(self.grille[k,:]) == 3 or np.sum(self.grille[:,k]) == 3:
+                return 1
+            if np.sum(self.grille[k,:]) == -3 or np.sum(self.grille[:,k]) == -3:
+                return -1
+        diag1 = self.grille[1,1]+self.grille[2,2]+self.grille[3,3]
+        diag2 = self.grille[1,3]+self.grille[2,2]+self.grille[3,1]
+        if diag1 == 3 or diag2 == 3:
+            return 1
+        if diag1 == -3 or diag2 == -3:
+            return -1
+        return 0
+    
+    def is_finished(self):
+        return self.winner() != 0
+
 
 
 if __name__ == "__main__":
 
     game = Tic_tac_toe()
     game.show()
-    """
     while not game.is_finished():
         game.next_move(input())
         game.show()
-    game.winner()"""
+    game.winner()
